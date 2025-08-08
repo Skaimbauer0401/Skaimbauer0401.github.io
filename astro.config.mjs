9ada43548c35fbcf66edb695d713461320f2977b
samue <samuel.bauer0401@outlook.de>
2025-08-09 01:46:12 +0200

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://skaimbauer0401.github.io/Website-Baumeister-Bauer/',
     base: '/Website-Baumeister-Bauer/',
 });
